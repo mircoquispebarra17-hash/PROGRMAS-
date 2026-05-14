ed13b8114e3f5c123d337c9a29108488c2a7135b
mircoquispebarra17-hash <mircoquispebarra17@gmail.com>
2026-05-14 15:35:09 -0500

    Add files via upload

--- import java.util.java
+++ import java.util.java
@@ -1,15 +1,32 @@
 import java.util.Scanner;
- class Lectura {
+ public class Comparaciones {
  public static void main(String[] args) {
  Scanner entrada = new Scanner(System.in);
+ int numero1 =5;
+ int numero2 =7;
 
- System.out.print("Ingrese un valor entero: ");
- int numero1 = entrada.nextInt();
+ System.out.print(" Ingrese un número entero: ");
+ numero1 = entrada.nextInt();
 
- System.out.print("Ingrese otro valor entero: ");
- int numero2 = entrada.nextInt();
+ System.out.print(" Ingrese otro número entero: ");
+ numero2 = entrada.nextInt();
 
- int suma = numero1 + numero2;
- System.out.println("La suma es " + suma);
+ System.out.print(numero1 +" es mayor a " + numero2 +": ");
+ System.out.println(numero1 > numero2 );
+
+ System.out.print(numero1 +" es menor a " + numero2 + ": ");
+ System.out.println(numero1 < numero2 );
+
+ System.out.print(numero1 +" es mayor o igual a "+numero2 + ": ");
+ System.out.println(numero1 >= numero2);
+
+ System.out.print(numero1 +" es menor o igual a "+numero2 + ": ");
+ System.out.println(numero1 <= numero2);
+
+ System.out.print(numero1 +" es igual a " + numero2 + ": ");
+ System.out.println(numero1 == numero2);
+
+ System.out.print(numero1 +" no es igual a " + numero2 + ": " );
+ System.out.println(numero1 != numero2 );
  } // Fin del método main
-} //  } // Fin de la clase Lectura
+ } // Fin de la clase Comparaciones
