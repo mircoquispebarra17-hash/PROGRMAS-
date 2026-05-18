9cab4be67ba511b266caa5478cb8ac99927f77f7
mircoquispebarra17-hash <mircoquispebarra17@gmail.com>
2026-05-18 09:03:04 -0500

    Add files via upload

--- InstruccionIf.java
+++ InstruccionIf.java
@@ -1,12 +1,13 @@
  import java.util.Scanner;
-public class InstruccionIf {
+ public class InstruccionIf {
  public static void main(String[] args) {
  Scanner entrada= new Scanner(System.in);
  System.out.print("Ingrese una edad: ");
  int edad = entrada.nextInt();
 
- if( edad >= 18 ){
- System.out.println("Mayor de edad"); }
+ {if( edad >= 18 ){
+ System.out.println("Mayor de edad");
+ }
  
  System.out.println("Adiós!");
  } // Fin del método main
--- InstruccionIfElse.java
+++ InstruccionIfElse.java
@@ -1,13 +1,18 @@
- import java.util.Scanner;
-public class InstruccionIf {
+import java.util.Scanner;
+ public class InstruccionIfElse {
  public static void main(String[] args) {
+
  Scanner entrada= new Scanner(System.in);
+
+
  System.out.print("Ingrese una edad: ");
  int edad = entrada.nextInt();
 
  if( edad >= 18 ){
- System.out.println("Mayor de edad"); }
- 
+ System.out.println("Mayor de edad");
+ }else{
+ System.out.println("Menor de edad");
+ }
  System.out.println("Adiós!");
  } // Fin del método main
- } // Fin de la clase InstruccionIf
+} // Fin de la clase InstruccionIfElse
